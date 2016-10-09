35cdbfa4ad5fbf7ceedb0d49f3f4f0dece3172e2
Pramod S Kumbhar <pramod.r.s.kumbhar@gmail.com>
2016-10-09 14:01:31 +0200

    bg-q update

--- bbpbgq/bbp.sh
+++ bbpbgq/bbp.sh
@@ -1,5 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name="build"
+#SBATCH --time=2:00:00
+#SBATCH --partition=interactive
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=16
+#SBATCH --account=proj16
+#SBATCH --exclusive
+
 set -e
-set -x
 
 spack uninstall -f -a -d -y mod2c
 spack uninstall -f -a -d -y nrnh5
@@ -12,7 +20,6 @@ spack uninstall -f -a -d -y neuron-nmodl
 spack install mod2c os=redhat6
 spack install reportinglib ^bgqmpi
 spack install nrnh5 +zlib ^bgqmpi
-
 spack install neuron +mpi +cross-compile ^bgqmpi ^neuron-nmodl os=redhat6
 spack install neurodamus ^neuron+mpi+cross-compile ^bgqmpi ^neuron-nmodl os=redhat6
 spack install coreneuron +hdf5 +neurodamus +report ^bgqmpi ^mod2c os=redhat6 ^neurodamus ^neuron+mpi+cross-compile ^neuron-nmodl os=redhat6 ^nrnh5+zlib
