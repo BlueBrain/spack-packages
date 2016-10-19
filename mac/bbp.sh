8db017bfe1c149b969adfef6fb9d5b6c60a24c2c
Pramod S Kumbhar <pramod.r.s.kumbhar@gmail.com>
2016-10-20 00:48:42 +0200

    reorganized bbp packages and only mac scripts are updated

--- mac/bbp.sh
+++ mac/bbp.sh
@@ -6,17 +6,24 @@ spack uninstall -y -f -d -a mod2c
 spack uninstall -y -f -d -a reportinglib
 spack uninstall -y -f -d -a neurodamus
 
+set -e
+set -x
+
 spack install mpich@3.2 %gcc@4.9.3
 spack install hdf5 +mpi
 spack install python
 
 spack install mod2c %gcc
 spack install nrnh5 %gcc ^mpich
+
 spack install reportinglib %gcc  ^mpich
-spack install neuron@master +mpi %gcc ^mpich
+
 spack install neuron@develop +mpi %gcc ^mpich
+spack install neuron@hdf +mpi %gcc ^mpich
+
 spack install neurodamus@master +compile %gcc ^mpich
-spack install neurodamus@coreneuronsetup +compile %gcc ^mpich
 spack install neurodamus@develop +compile %gcc ^mpich
-spack install coreneuron +report %gcc ^mpich
+spack install neurodamus@hdf +compile %gcc ^mpich
+
 spack install coreneuron@develop +report %gcc ^mpich
+spack install coreneuron@hdf +report %gcc ^mpich
