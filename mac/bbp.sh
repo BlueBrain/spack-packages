2f605ef2b6d9f5b0aa376aed5b736ab7a6f0f942
Pramod S Kumbhar <pramod.r.s.kumbhar@gmail.com>
2016-10-23 20:28:52 +0200

    built on osx with clang and gcc

--- mac/bbp.sh
+++ mac/bbp.sh
@@ -1,29 +1,73 @@
-spack uninstall -y -f -d -a neuron
-spack uninstall -y -f -d -a coreneuron
-spack uninstall -y -f -d -a nrnh5
-spack uninstall -y -f -d -a nrnh5
-spack uninstall -y -f -d -a mod2c
-spack uninstall -y -f -d -a reportinglib
-spack uninstall -y -f -d -a neurodamus
+#!/bin/bash
 
-set -e
 set -x
 
-spack install mpich@3.2 %gcc@4.9.3
-spack install hdf5 +mpi
-spack install python
+#### LIST OF PACKAGES ####
+dev_packages=(
+    'mod2c@develop'
+    'mod2c@github'
 
-spack install mod2c %gcc
-spack install nrnh5 %gcc ^mpich
+    'nrnh5'
 
-spack install reportinglib %gcc  ^mpich
+    'neuron@develop'
+    'neuron@hdf'
 
-spack install neuron@develop +mpi %gcc ^mpich
-spack install neuron@hdf +mpi %gcc ^mpich
+    'reportinglib'
 
-spack install neurodamus@master +compile %gcc ^mpich
-spack install neurodamus@develop +compile %gcc ^mpich
-spack install neurodamus@hdf +compile %gcc ^mpich
+    'neurodamus@master'
+    'neurodamus@develop'
+    'neurodamus@hdf'
 
-spack install coreneuron@develop +report %gcc ^mpich
-spack install coreneuron@hdf +report %gcc ^mpich
+    'coreneuron@develop'
+    'coreneuron@github'
+    'coreneuron@hdf'
+)
+
+compilers=(
+    '%gcc'
+    '%clang'
+)
+
+
+#### WE WILL INSTALL PYTHON AND HDF5 ONCE
+dependency_install() {
+	for compiler in "${compilers[@]}"
+    do
+		  spack install python $compiler
+		  spack install mpich $compiler
+		  spack install hdf5 $compiler
+	done
+}
+
+##### UNINSTALL PACKAGE #####
+uninstall_package() {
+	for package in "${dev_packages[@]}"
+    do
+		  spack uninstall -a -f -d -y $package
+	done
+}
+
+#if any inconsistent packages
+spack reindex
+
+# uninstall packages
+uninstall_package
+
+# install dependency packages like python and hdf5
+dependency_install
+
+# stop if iany package installation fails
+set -e
+
+# for every compiler, build each package
+for compiler in "${compilers[@]}"
+do
+	for package in "${dev_packages[@]}"
+    do
+        # neurodamus doesnt build with Clanf compiler
+        if [[ $compiler == *"clang"* ]] && [[ $package == *"neurodamus"* ]]; then
+            continue
+        fi
+        spack install $package $compiler
+    done
+done
