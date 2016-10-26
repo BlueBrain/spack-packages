cc9c850aa57de01b1892d3702ee334739a1ccc0b
Pramod S Kumbhar <pramod.r.s.kumbhar@gmail.com>
2016-10-27 01:03:43 +0200

    Built stack with GCC, Intel and PGI on bbpviz cluster

--- bbpviz/bbp.sh
+++ bbpviz/bbp.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name="build-neuron-stack"
-#SBATCH --time=2:00:00
+#SBATCH --job-name="neuron-coreneuron-stack"
+#SBATCH --time=4:00:00
 #SBATCH --partition=interactive
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=16
 #SBATCH --account=proj16
 #SBATCH --exclusive
-
-#Module issue
-export LD_LIBRARY_PATH=/gpfs/bbp.cscs.ch/apps/viz/intel2017/compilers_and_libraries_2017.0.098/linux/mpi/intel64/lib/:$LD_LIBRARY_PATH
 
 ##### UNINSTALL PACKAGE #####
 uninstall_package() {
@@ -27,26 +24,27 @@ dependency_install() {
     spack install --dirty hdf5 +mpi %intel ^intelmpi
 }
 
+
 #### COMPILER TOOLCHAINS ####
 compiler_with_mpi=(
-    '%intel ^intelmpi'
+    '%pgi ^mpich'
     '%gcc ^mvapich2'
+    '%intel ^intelmpi'
 )
 
 #in case there are inconsistencies
 spack reindex
 
+# uninstall all packages
 uninstall_package
+
+# uninstall hdf5 : not often!
 #spack uninstall -a -f -d -y hdf5
 
 #stop on error
 set -e
 
 dependency_install
-
-# hdf5 issue
-spack load hdf5 +mpi %gcc ^mvapich2
-spack load hdf5 +mpi %intel ^intelmpi
 
 #packages without MPI dependency
 spack install --dirty mod2c@develop %intel
@@ -55,28 +53,28 @@ spack install --dirty mod2c@github %intel
 spack install --dirty mod2c@develop %gcc
 spack install --dirty mod2c@github %gcc
 
+spack install --dirty mod2c@develop %pgi
+spack install --dirty mod2c@github %pgi
 
 for compiler_mpi in "${compiler_with_mpi[@]}"
 do
-    spack install -v --dirty nrnh5@develop $compiler_mpi
+    # we dont have hdf5 compiled with PGI
+    if [[ $compiler_mpi != *"pgi"* ]]; then
+        spack install --dirty nrnh5@develop $compiler_mpi
+        spack install --dirty neuron@hdf +mpi $compiler_mpi
+    fi
 
-    spack install -v --dirty neuron@develop +mpi $compiler_mpi
-    spack install -v --dirty neuron@hdf +mpi $compiler_mpi
+    spack install --dirty neuron@develop +mpi $compiler_mpi
+    spack install --dirty reportinglib $compiler_mpi
 
-    spack install -v --dirty reportinglib $compiler_mpi
+    # we dont have hdf5 compiled with PGI
+    if [[ $compiler_mpi != *"pgi"* ]]; then
+        spack install --dirty neurodamus@master +compile $compiler_mpi
+        spack install --dirty neurodamus@develop +compile $compiler_mpi
+        spack install --dirty neurodamus@hdf +compile $compiler_mpi
+        spack install --dirty coreneuron@hdf +mpi +report $compiler_mpi
+    fi
 
-    spack install -v --dirty neurodamus@master +compile $compiler_mpi
-    spack install -v --dirty neurodamus@develop +compile $compiler_mpi
-    spack install -v --dirty neurodamus@hdf +compile $compiler_mpi
-
-    spack install -v --dirty coreneuron@develop +report $compiler_mpi
-    spack install -v --dirty coreneuron@github +report $compiler_mpi
-    spack install -v --dirty coreneuron@hdf +report $compiler_mpi
+    spack install --dirty coreneuron@develop +mpi +report $compiler_mpi
+    spack install --dirty coreneuron@github +mpi +report $compiler_mpi
 done
-
-spack install -v --dirty mod2c@develop %pgi
-spack install -v --dirty coreneuron@develop +report %pgi ^mvapich2
-
-spack install -v --dirty mod2c@github %pgi
-#need to push the fix to github repos
-#spack install --dirty coreneuron@github +report %pgi ^mvapich2
