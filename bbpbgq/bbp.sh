5a9d55449e32cc0a63c81ac266712872d7c28d05
Pramod S Kumbhar <pramod.r.s.kumbhar@gmail.com>
2016-11-01 21:07:11 +0100

    new script for installing entire stack on BG-Q

--- bbpbgq/bbp.sh
+++ bbpbgq/bbp.sh
@@ -1,31 +1,62 @@
 #!/bin/bash
-#SBATCH --job-name="build"
-#SBATCH --time=2:00:00
+#SBATCH --job-name="neuron-coreneuron-stack"
+#SBATCH --time=4:00:00
 #SBATCH --partition=interactive
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=16
 #SBATCH --account=proj16
 #SBATCH --exclusive
 
-#nmodl is still issue
+##### EXTRA OPTIONS FOR INSTALL #####
+extra_opt="--log-format=junit"
 
-#uninstall all packages
-spack uninstall -f -a -d -y mod2c
-spack uninstall -f -a -d -y nrnh5
-spack uninstall -f -a -d -y reportinglib
-spack uninstall -f -a -d -y neuron-nmodl
-spack uninstall -f -a -d -y neuron
-spack uninstall -f -a -d -y coreneuron
-spack uninstall -f -a -d -y neurodamus
+#in case there are inconsistencies
+spack reindex
 
+set -x
+
+#### LIST OF PACKAGES ####
+dev_packages=(
+    'mod2c@develop os=redhat6'
+    'mod2c@github os=redhat6'
+
+    'nrnh5'
+
+    'neuron@develop'
+    'neuron@hdf'
+
+    'reportinglib'
+
+    'neurodamus@hdf'
+    'neurodamus@develop'
+    'neurodamus@master'
+
+    'coreneuron@develop'
+    'coreneuron@github'
+    'coreneuron@hdf'
+    'neuronperfmodels'
+    'coreneuron@perfmodels'
+)
+
+##### UNINSTALL PACKAGE #####
+uninstall_package() {
+    for package in "${dev_packages[@]}"
+    do
+        spack uninstall -a -f -d -y $package
+    done
+}
+
+#if any inconsistent packages
+spack reindex
+
+# uninstall all packages
+uninstall_package
+
+# stop if iany package installation fails
 set -e
 
-spack install mod2c os=redhat6
-spack install nrnh5 +zlib ^mpich
-spack install reportinglib ^mpich
-spack install neuron-nmodl os=redhat6
-spack install neuron +mpi +hdf5 +cross-compile ^mpich ^neuron-nmodl os=redhat6
-spack install neurodamus +compile ^mpich
-spack install neurodamus@coreneuronsetup +compile ^mpich
-spack install coreneuron +neurodamus +report ^mpich ^mod2c os=redhat6 ^neurodamus
-spack install coreneuron@develop +neurodamus +report ^mpich ^nrnh5+zlib ^mod2c os=redhat6
+# for every compiler, build each package
+for package in "${dev_packages[@]}"
+do
+    spack install $extra_opt $package $compiler
+done
