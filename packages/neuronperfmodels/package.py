36bfadeb94a973e5f00f8f9ebf4a8941b840324b
Pramod S Kumbhar <pramod.r.s.kumbhar@gmail.com>
2016-11-15 18:05:31 +0100

    Don't skip neuronperfmodels@neuron installation with PGI as issue is fixed

--- packages/neuronperfmodels/package.py
+++ packages/neuronperfmodels/package.py
@@ -74,11 +74,6 @@ class Neuronperfmodels(Package):
         self.check_install()
 
     def build_neurodamus(self, spec, prefix):
-        # skip installation if pgi compiler is used
-        if spec.satisfies('%pgi'):
-            print "INFO : Skipping neurodamus build with PGI!"
-            return
-
         src = '%s/neurodamus/lib' % self.stage.source_path
         dest = '%s/neurodamus/lib' % prefix
         install_tree(src, dest, symlinks=False)
