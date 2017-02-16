aa58d63a28ed219e453f4d943b4ed5040443a386
Pramod S Kumbhar <pramod.r.s.kumbhar@gmail.com>
2017-02-16 11:04:01 +0000

    Add latest savestate branch as new version for NEURODAMUS

--- packages/neurodamus/package.py
+++ packages/neurodamus/package.py
@@ -31,6 +31,7 @@ class Neurodamus(Package):
     version('gpu',     git=url, branch='sandbox/kumbhar/coreneuronsetup_gpu')
     version('plasticity',     git=url, branch='sandbox/chindemi/glusynapse-master')
     version('oldplasticity',     git=url, branch='sandbox/kumbhar/savestate_devel')
+    version('saveupdate', git=url, branch='sandbox/king/saveupdate')
 
     variant('compile', default=True, description='Compile and create executable using nrnivmodl')
     variant('profile', default=False, description="Enable profiling using Tau")
