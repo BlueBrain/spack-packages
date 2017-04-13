7b664ae75ecff50f1cd8cf194f2a212ca78f517b
Pramod S Kumbhar <pramod.r.s.kumbhar@gmail.com>
2017-04-13 23:43:18 +0200

    Changes with spack llnl develop branche.

--- daint/bbp.sh
+++ daint/bbp.sh
@@ -1,26 +1,51 @@
+#!/bin/bash
+dev_packages=(
+    'neuronperfmodels'
+    'coreneuron@perfmodels +report ^reportinglib+static'
+)
+
+#    'neuronperfmodels +profile'
+#    'coreneuron@perfmodels +profile +report ^reportinglib+static'
+
+compilers=(
+    '%cce@8.5.5'
+    '%intel'
+)
+
+##### UNINSTALL PACKAGE #####
+uninstall_package() {
+    for package in "${dev_packages[@]}"
+    do
+        spack uninstall -a -f -R -y $package
+    done
+}
+
+#if any inconsistent packages
+spack reindex
+
+# uninstall all packages
+uninstall_package
+
+# stop if iany package installation fails
 set -e
-set -x
 
-spack uninstall -f -a -d -y mod2c
-spack uninstall -f -a -d -y nrnh5
-spack uninstall -f -a -d -y neuron
-spack uninstall -f -a -d -y reportinglib
-spack uninstall -f -a -d -y coreneuron
-spack uninstall -f -a -d -y neurodamus
-spack uninstall -f -a -d -y neuron-nmodl
+#export TAU_OPTIONS='-optPDTInst -optNoCompInst -optRevert -optPdtCOpts="-D_CRAYC=1" -optTauSelectFile=~/spackconfig/nrnperfmodels.tau'
 
-spack install mod2c os=SuSE11
-spack install reportinglib ^craympi
-spack install nrnh5 +zlib ^craympi
+# for every compiler, build each package
+for compiler in "${compilers[@]}"
+do
+    for package in "${dev_packages[@]}"
+    do
+        spack install $package $compiler
+    done
+done
 
-spack install -v coreneuron%intel +hdf5 ~neurodamus +report ^craympi os=CNL ^nrnh5+zlib%intel os=CNL ^mod2c%gcc os=SuSE11
+
 
 exit 0
 
 #still doesnt work
 spack install neuron +mpi +cross-compile ^craympi ^neuron-nmodl os=SuSE11
 spack install neurodamus ^neuron+mpi+cross-compile ^craympi ^neuron-nmodl os=SuSE11
-spack install coreneuron +hdf5 +neurodamus +report ^craympi ^mod2c os=SuSE11 ^neurodamus ^neuron+mpi+cross-compile ^neuron-nmodl os=SuSE11 ^nrnh5+zlib
-
 #also works
 spack install -v coreneuron%intel +hdf5 ~neurodamus +report ^mpich@7.2.2 os=CNL ^nrnh5+zlib%intel os=CNL ^mod2c%gcc os=SuSE11
