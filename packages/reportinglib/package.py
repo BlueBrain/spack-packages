fe39e258be5b961294eb3fc41520853d97640e38
Pramod S Kumbhar <pramod.r.s.kumbhar@gmail.com>
2017-07-25 15:26:10 +0000

    Added new version of neurodamus to test branch of reportinglib which gathers the header buffer to avoid mpi memory issues on bg-q.

--- packages/reportinglib/package.py
+++ packages/reportinglib/package.py
@@ -25,6 +25,7 @@ class Reportinglib(Package):
 
     version('develop', git=url, preferred=True)
     version('memleakfix', git=url, branch='sandbox/kumbhar/memleakfix')
+    version('gather', git=url, branch='sandbox/king/gatherMappingREP-31')
 
     variant('profile', default=False, description="Enable profiling using Tau")
     variant('static',  default=False, description="Build static library")
