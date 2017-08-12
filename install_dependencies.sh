85bc8462bdc8de034e0ef03978c0768aa752d2d7
Pramod S Kumbhar <pramod.r.s.kumbhar@gmail.com>
2017-08-12 21:15:20 +0200

    more cleanup

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
-# This configuration file was taken from the mpi4py and Julia project
-# and then modified for spack-packages repository
 
 set -e
 set -x
 
-MPI_IMPL="$1"
+MPI_LIB_NAME="$1"
 os=`uname`
-
-echo "---------> $MPI :: $MPI_IMPL"
 
 case "$os" in
     Darwin)
@@ -17,7 +13,7 @@ case "$os" in
         brew tap homebrew/science
         brew install lmod
 
-        case "$MPI_IMPL" in
+        case "$MPI_LIB_NAME" in
             mpich|mpich3)
                 brew install mpich
                 ;;
@@ -25,7 +21,7 @@ case "$os" in
                 brew install openmpi
                 ;;
             *)
-                echo "ERROR: Unknown MPI Implementation: $MPI_IMPL"
+                echo "ERROR: Unknown MPI Implementation: $MPI_LIB_NAME"
                 exit 1
                 ;;
         esac
@@ -34,28 +30,19 @@ case "$os" in
     Linux)
         sudo apt-get update -q
 
-        # TODO: for bug in Ubuntu 14.04
+        # TODO: workaround for bug in Ubuntu 14.04
         # check https://bugs.launchpad.net/ubuntu/+source/python2.7/+bug/1115466
         sudo ln -s /usr/lib/python2.7/plat-*/_sysconfigdata_nd.py /usr/lib/python2.7/
 
-        case "$MPI_IMPL" in
+        case "$MPI_LIB_NAME" in
             mpich|mpich3)
                 sudo apt-get install -y gfortran libcr0 default-jdk hwloc libmpich10 mpich libmpich-dev
-                #wget -q http://de.archive.ubuntu.com/ubuntu/pool/universe/m/mpich/mpich_3.0.4-6ubuntu1_amd64.deb
-                #sudo dpkg -i ./mpich_3.0.4-6ubuntu1_amd64.deb
                 ;;
             openmpi)
-                #sudo apt-get install -y gfortran
-                #wget --no-check-certificate https://www.open-mpi.org/software/ompi/v1.10/downloads/openmpi-1.10.2.tar.gz
-                #tar -zxf openmpi-1.10.2.tar.gz
-                #cd openmpi-1.10.2
-                #sh ./configure --prefix=$HOME/OpenMPI > /dev/null
-                #make -j > /dev/null
-                #make install > /dev/null
                 sudo apt-get install -y openmpi-bin libopenmpi-dev
                 ;;
             *)
-                echo "ERROR: Unknown MPI Implementation: $MPI_IMPL"
+                echo "ERROR: Unknown MPI Implementation: $MPI_LIB_NAME"
                 exit 1
                 ;;
         esac
