b5f42c1917dc9db193de2ae66df71d751ce2b38c
Pramod S Kumbhar <pramod.r.s.kumbhar@gmail.com>
2017-08-12 10:37:16 +0200

    udpate travis

--- .travis.spack_setup.sh
+++ .travis.spack_setup.sh
@@ -1,33 +1,25 @@
 #!/bin/bash
 set -x
 
-export WORK_DIR=`pwd`
-
 # spack (personal fork)
 git clone https://github.com/pramodskumbhar/spack.git
 cd spack && git checkout upstream
 
-# spack (personal packages)
-git clone https://github.com/pramodskumbhar/spack-packages.git
+echo $PATH
+env
 
-export SPACK_ROOT=`pwd`
-
-#setup spack variables
-#echo "" >> $HOME/.bashrc
-#echo "#Setup SPACK path" >> $HOME/.bashrc
-export SPACK_ROOT=${SPACK_ROOT}
-export PATH=$SPACK_ROOT/bin:$PATH
 source $SPACK_ROOT/share/spack/setup-env.sh
-source  /etc/profile.d/modules.sh
+export PATH=$SPACK_ROOT/bin:$PATH
 
-spack repo add --scope site `pwd`/spack-packages
-
-cd $WORK_DIR
-
+cd ..
 mkdir -p $HOME/.spack/
 cp .travis.packages.yaml $HOME/.spack/packages.yaml
 
+# add
+spack repo add --scope site .
+
+cd $WORK_DIR
+
 spack compilers
 spack spec neuron
-spack spec coreneuron
 
