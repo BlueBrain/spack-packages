17bc21172ca6c97b978d9f1dafd6b2ad33677b76
Pramod S Kumbhar <pramod.r.s.kumbhar@gmail.com>
2017-08-18 08:55:00 +0200

    Cleanup

--- packages/lengine/package.py
+++ packages/lengine/package.py
@@ -22,8 +22,6 @@
 # License along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
-import string
-import os
 from spack import *
 
 
@@ -48,6 +46,7 @@ class Lengine(Package):
     variant('docs',         default=False, description="Enable Python Docs")
     variant('syn2',         default=False, description="Enable Syn2 reader")
     variant('knl',          default=False, description="Build for KNL")
+    variant('profile',      default=False, description="Build with TAU")
     variant('threading',    default='tbb', values=('tbb', 'omp', 'seq'),
             multi=False, description="Threading backends"
     )
@@ -68,8 +67,9 @@ class Lengine(Package):
     depends_on('syn2', when='+syn2')
     depends_on('highfive', when='+syn2')
     depends_on('py-sphinx', when='+docs')
+    depends_on('tau', when='+profile')
 
-    def get_optimization_level(self):
+    def get_optimization_flags(self):
         flags = "-g -O2"
 
         if self.spec.satisfies('%intel'):
@@ -89,13 +89,13 @@ class Lengine(Package):
             c_compiler = spack_cc
             cxx_compiler = spack_cxx
 
-            optflag = self.get_optimization_level()
+            optflag = self.get_optimization_flags()
 
             if self.spec.satisfies('+profile'):
                 c_compiler = 'tau_cc'
                 cxx_compiler = 'tau_cxx'
-            # for bg-q, our cmake is not setup properly
-            elif 'bgq' in self.spec.architecture and self.spec.satisfies('+mpi'):
+            # for bg-q : c, cxx wrappers should be mpi wrappers
+            if 'bgq' in self.spec.architecture and self.spec.satisfies('+mpi'):
                     c_compiler = self.spec['mpi'].mpicc
                     cxx_compiler = self.spec['mpi'].mpicxx
 
@@ -103,58 +103,4 @@ class Lengine(Package):
                              '-DCMAKE_C_FLAGS=%s' % optflag,
                              '-DCMAKE_CXX_FLAGS=%s' % optflag,
                              '-DCMAKE_C_COMPILER=%s' % c_compiler,
-                             '-DCMAKE_CXX_COMPILER=%s' % cxx_compiler,
-                             '-Dwith-ltdl=OFF',
-                             '-Dwith-readline=OFF']
-
-            # not sure the -Dwith-optimize=flags option is required if we already set CMAKE_C_FLAGS above
-            # but just to be safe
-            semicolon_separated_optflag = optflag.translate(string.maketrans(' ', ';'))
-            cmake_options.extend(['-Dwith-optimize=' + semicolon_separated_optflag])
-
-            if self.spec.satisfies('~python'):
-                cmake_options.extend(['-Dwith-python=OFF'])
-                cmake_options.extend(['-Dcythonize-pynest=OFF'])
-            else:
-                cmake_options.extend(['-Dwith-python=ON'])
-                cmake_options.extend(['-Dcythonize-pynest=ON'])
-
-            if self.spec.satisfies('~gsl'):
-                cmake_options.extend(['-Dwith-gsl=OFF'])
-            else:
-                cmake_options.extend(['-Dwith-gsl=ON'])
-
-            if self.spec.satisfies('~mpi'):
-                cmake_options.extend(['-Dwith-mpi=OFF'])
-            else:
-                cmake_options.extend(['-Dwith-mpi=ON'])
-
-            if self.spec.satisfies('~openmp'):
-                cmake_options.extend(['-Dwith-openmp=OFF'])
-            else:
-                cmake_options.extend(['-Dwith-openmp=ON'])
-
-            if self.spec.satisfies('~static'):
-                cmake_options.extend(['-Dstatic-libraries=OFF'])
-            else:
-                cmake_options.extend(['-Dstatic-libraries=ON'])
-
-            cmake('..', *cmake_options)
-            self.profiling_wrapper_on()
-            make()
-            make('install')
-            self.profiling_wrapper_off()
-
-    def setup_environment(self, spack_env, run_env):
-        exe = '%s/nest' % self.prefix.bin
-        run_env.set('NEST_EXE', exe)
-
-        # for mvapich2 module we need to setup MV2_ENABLE_AFFINITY
-        # env variable to turn on multi-threading support
-        run_env.set('MV2_ENABLE_AFFINITY', '0')
-
-        # How to get python version specified by user???
-        if self.spec.satisfies('+python'):
-            py_version_string = 'python{0}'.format(self.spec['python'].version.up_to(2))
-            nest_package_py_path = os.path.join(self.prefix.lib64, py_version_string, 'site-packages')
-            run_env.prepend_path('PYTHONPATH', nest_package_py_path)
+                             '-DCMAKE_CXX_COMPILER=%s' % cxx_compiler]
