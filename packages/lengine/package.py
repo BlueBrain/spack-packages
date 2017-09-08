452aba6e98f3abb0ea57e9edf3bb7d324dea64dd
Schumann Till <Till.Schumann@epfl.ch>
2017-09-08 14:33:57 +0200

    synapse-tool and le config adapted

--- packages/lengine/package.py
+++ packages/lengine/package.py
@@ -25,7 +25,7 @@
 from spack import *
 
 
-class Lengine(Package):
+class Lengine(CMakePackage):
     """Testing framework for LE"""
 
     homepage = "ssh://bbpcode.epfl.ch/hpc/learning_engine"
@@ -33,49 +33,38 @@ class Lengine(Package):
 
     version('develop', git=url)
 
-    variant('sandbox',      default=True,  description="Build the main (sandbox)")
-    variant('draft',        default=True,  description="Build first draft")
     variant('tests',        default=True,  description="Build the regression tests")
-    variant('mpi',          default=True, description="Enable MPI")
-    variant('graph',        default=True, description="Enable boost graph")
     variant('benchmark',    default=True, description="Enable benchmarks")
-    variant('gsl',          default=True,  description="Enable GSL")
     variant('pybinding',    default=True, description="Enable Python Binding")
     variant('cuda',         default=False, description="Enable CUDA")
-    variant('slurm',        default=False, description="Enable Slurm")
     variant('docs',         default=False, description="Enable Python Docs")
     variant('syn2',         default=False, description="Enable Syn2 reader")
     variant('knl',          default=False, description="Build for KNL")
-    variant('profile',      default=False, description="Build with TAU")
 
     variant('threading',    default='tbb', values=('tbb', 'omp', 'seq'),
             multi=False, description="Threading backends"
     )
+
     variant('random',       default='standard', values=('mkl', 'standard'),
             multi=False, description="Random number generators"
     )
 
-    depends_on('cmake@3.1:', type='build')
-    depends_on('mpi', when='+mpi')
-    depends_on('gsl', when='+gsl')
-    depends_on('gsl', when='+draft')
+    variant('precision',    default='double', values=('double', 'float'),
+            multi=False, description="Floating Point Precision"
+    )
+
     depends_on('python@2.7:', when='+pybinding')
     depends_on('py-cython', when='+pybinding')
+    #depends_on('py-numpy', when='+pybinding')
     depends_on("intel-tbb", when='threading=tbb')
-    depends_on("slurm", type='build', when='+slurm')
     depends_on("cuda", when='+cuda')
-    depends_on("boost@1.52:+graph+test+system+program_options")
-    depends_on('syn2', when='+syn2')
-    depends_on('highfive', when='+syn2')
+    depends_on("boost@1.52:")
+    depends_on('synapse-tool', when='+syn2')
+    depends_on('highfive@master', when='+syn2')
     depends_on('py-sphinx', when='+docs')
-    depends_on('tau', when='+profile')
 
     conflicts('%gcc', when='random=mkl')
     conflicts('%clang', when='random=mkl')
-
-    def profiling_wrapper_on(self):
-        if self.spec.satisfies('+profile'):
-            os.environ["USE_PROFILER_WRAPPER"] = "1"
 
     def get_optimization_flags(self):
         flags = "-g -O2"
@@ -88,43 +77,16 @@ class Lengine(Package):
 
         return flags
 
-    def install(self, spec, prefix):
+    def cmake_args(self):
 
-        build_dir = "spack-build-%s" % spec.version
+        spec = self.spec
 
-        with working_dir(build_dir, create=True):
-            c_compiler = spack_cc
-            cxx_compiler = spack_cxx
+        with working_dir("build", create=True):
 
             optflag = self.get_optimization_flags()
 
-            if self.spec.satisfies('+profile'):
-                c_compiler = 'tau_cc'
-                cxx_compiler = 'tau_cxx'
-            # for bg-q : c, cxx wrappers should be mpi wrappers
-            if 'bgq' in self.spec.architecture and self.spec.satisfies('+mpi'):
-                    c_compiler = self.spec['mpi'].mpicc
-                    cxx_compiler = self.spec['mpi'].mpicxx
-
-            args = ['-DCMAKE_INSTALL_PREFIX:PATH=%s' % prefix,
-                    '-DCMAKE_C_FLAGS=%s' % optflag,
-                    '-DCMAKE_CXX_FLAGS=%s' % optflag,
-                    '-DCMAKE_C_COMPILER=%s' % c_compiler,
-                    '-DCMAKE_CXX_COMPILER=%s' % cxx_compiler]
-
-            """ TODO: we should use short versions but they exceeed 80 column limit
-	    args.extend['-DLEARNING_ENGINE_SANDBOX=%s' % ('ON' if spec.satisfies('+sandbox') else 'OFF'),
-            """
-
-            if spec.satisfies('+sandbox'):
-                args.append('-DLEARNING_ENGINE_SANDBOX=ON')
-            else:
-                args.append('-DLEARNING_ENGINE_SANDBOX=OFF')
-
-            if spec.satisfies('+draft'):
-                args.append('-DLEARNING_ENGINE_DRAFT=ON')
-            else:
-                args.append('-DLEARNING_ENGINE_DRAFT=OFF')
+            args = ['-DCMAKE_C_FLAGS=%s' % optflag,
+                    '-DCMAKE_CXX_FLAGS=%s' % optflag]
 
             if spec.satisfies('+tests'):
                 args.append('-DLEARNING_ENGINE_TESTS=ON')
@@ -136,35 +98,15 @@ class Lengine(Package):
             else:
                 args.append('-DLEARNING_ENGINE_CUDA=OFF')
 
-            if spec.satisfies('+mpi'):
-                args.append('-DLEARNING_ENGINE_MPI=ON')
-            else:
-                args.append('-DLEARNING_ENGINE_MPI=OFF')
-
-            if spec.satisfies('+graph'):
-                args.append('-DLEARNING_ENGINE_GRAPH=ON')
-            else:
-                args.append('-DLEARNING_ENGINE_GRAPH=OFF')
-
             if spec.satisfies('+benchmark'):
                 args.append('-DLEARNING_ENGINE_BENCHMARK=ON')
             else:
                 args.append('-DLEARNING_ENGINE_BENCHMARK=OFF')
 
-            if spec.satisfies('+gsl'):
-                args.append('-DLEARNING_ENGINE_GSL=ON')
-            else:
-                args.append('-DLEARNING_ENGINE_GSL=OFF')
-
             if spec.satisfies('+pybinding'):
                 args.append('-DLEARNING_ENGINE_PYTHON_BINDING=ON')
             else:
                 args.append('-DLEARNING_ENGINE_PYTHON_BINDING=OFF')
-
-            if spec.satisfies('+slurm'):
-                args.append('-DLEARNING_ENGINE_SLURM=ON')
-            else:
-                args.append('-DLEARNING_ENGINE_SLURM=OFF')
 
             if spec.satisfies('+docs'):
                 args.append('-DLEARNING_ENGINE_SPHINX=ON')
@@ -176,19 +118,8 @@ class Lengine(Package):
             else:
                 args.append('-DLEARNING_ENGINE_SYN2=OFF')
 
-            if spec.satisfies('+syn2'):
-                args.append('-DLEARNING_ENGINE_SYN2=ON')
-            else:
-                args.append('-DLEARNING_ENGINE_SYN2=OFF')
-
             args.append('-DOPT_THREAD=%s' % spec.variants['threading'].value)
             args.append('-DOPT_RANDOM=%s' % spec.variants['random'].value)
+            args.append('-DOPT_PRECISION=%s' % spec.variants['precision'].value)
 
-            cmake('..', *args)
-            self.profiling_wrapper_on()
-            make()
-            make('install')
-
-    def setup_environment(self, spack_env, run_env):
-        exe = '%s/bench_brunel' % self.prefix.bin
-        run_env.set('BENCH_BRUNEL', exe)
+            return args
