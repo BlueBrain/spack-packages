471a6ab26121f4759e47ef732b5c078756ca07ad
Pramod S Kumbhar <pramod.r.s.kumbhar@gmail.com>
2017-09-12 20:51:01 +0200

    Lengine package (#31)
    * First draft for LE and dependency package syntool

---
+++ packages/learningengine/package.py
@@ -0,0 +1,117 @@
+##############################################################################
+# Copyright (c) 2013-2016, Lawrence Livermore National Security, LLC.
+# Produced at the Lawrence Livermore National Laboratory.
+#
+# This file is part of Spack.
+# Created by Todd Gamblin, tgamblin@llnl.gov, All rights reserved.
+# LLNL-CODE-647188
+#
+# For details, see https://github.com/llnl/spack
+# Please also see the NOTICE and LICENSE files for our notice and the LGPL.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License (as
+# published by the Free Software Foundation) version 2.1, February 1999.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms and
+# conditions of the GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+##############################################################################
+from spack import *
+
+
+class Learningengine(CMakePackage):
+    """Testing framework for LE"""
+
+    homepage = "ssh://bbpcode.epfl.ch/hpc/learning_engine"
+    url      = "ssh://bbpcode.epfl.ch/hpc/learning_engine"
+
+    version('develop', git=url)
+
+    variant('tests',        default=True,  description="Build the regression tests")
+    variant('benchmark',    default=True, description="Enable benchmarks")
+    variant('pybinding',    default=True, description="Enable Python Binding")
+    variant('cuda',         default=False, description="Enable CUDA")
+    variant('docs',         default=False, description="Enable Python Docs")
+    variant('syn2',         default=False, description="Enable Syn2 reader")
+    variant('knl',          default=False, description="Build for KNL")
+
+    variant('threading',    default='tbb', values=('tbb', 'omp', 'seq'),
+            multi=False, description="Threading backends"
+    )
+
+    variant('random',       default='standard', values=('mkl', 'standard'),
+            multi=False, description="Random number generators"
+    )
+
+    variant('precision',    default='double', values=('double', 'float'),
+            multi=False, description="Floating Point Precision"
+    )
+
+    depends_on("boost@1.52:")
+    depends_on("cuda",                when='+cuda')
+    depends_on('syntool',             when='+syn2')
+    depends_on("intel-tbb",           when='threading=tbb')
+    depends_on('py-sphinx',           when='+docs')
+    depends_on('py-cython',           when='+pybinding')
+    depends_on('python@2.7:',         when='+pybinding')
+    depends_on('highfive@master~mpi', when='+syn2')
+
+    conflicts('%gcc', when='random=mkl')
+    conflicts('%clang', when='random=mkl')
+
+    def get_optimization_flags(self):
+        flags = "-g -O2"
+        if self.spec.satisfies('%intel'):
+            flags += ' -qopt-report=5'
+        if self.spec.satisfies('+knl'):
+            flags = ' -xmic-avx512'
+        return flags
+
+    def cmake_args(self):
+        spec = self.spec
+        optflag = self.get_optimization_flags()
+
+        args = ['-DCMAKE_C_FLAGS=%s' % optflag,
+                '-DCMAKE_CXX_FLAGS=%s' % optflag]
+
+        if spec.satisfies('+cuda'):
+            args.append('-DLEARNING_ENGINE_CUDA=ON')
+        else:
+            args.append('-DLEARNING_ENGINE_CUDA=OFF')
+
+        if spec.satisfies('+benchmark'):
+            args.append('-DLEARNING_ENGINE_BENCHMARK=ON')
+        else:
+            args.append('-DLEARNING_ENGINE_BENCHMARK=OFF')
+
+        if spec.satisfies('+pybinding'):
+            args.append('-DLEARNING_ENGINE_PYTHON_BINDING=ON')
+        else:
+            args.append('-DLEARNING_ENGINE_PYTHON_BINDING=OFF')
+
+        if spec.satisfies('+docs'):
+            args.append('-DLEARNING_ENGINE_SPHINX=ON')
+        else:
+            args.append('-DLEARNING_ENGINE_SPHINX=OFF')
+
+        if spec.satisfies('+syn2'):
+            args.append('-DLEARNING_ENGINE_SYN2=ON')
+        else:
+            args.append('-DLEARNING_ENGINE_SYN2=OFF')
+
+        if spec.satisfies('+tests'):
+            args.append('-DLEARNING_ENGINE_TESTS=ON')
+        else:
+            args.append('-DLEARNING_ENGINE_TESTS=OFF')
+
+        args.append('-DOPT_THREAD=%s' % spec.variants['threading'].value)
+        args.append('-DOPT_RANDOM=%s' % spec.variants['random'].value)
+        args.append('-DOPT_PRECISION=%s' % spec.variants['precision'].value)
+
+        return args
---
+++ packages/syntool/package.py
@@ -0,0 +1,66 @@
+##############################################################################
+# Copyright (c) 2013-2017, Lawrence Livermore National Security, LLC.
+# Produced at the Lawrence Livermore National Laboratory.
+#
+# This file is part of Spack.
+# Created by Todd Gamblin, tgamblin@llnl.gov, All rights reserved.
+# LLNL-CODE-647188
+#
+# For details, see https://github.com/llnl/spack
+# Please also see the NOTICE and LICENSE files for our notice and the LGPL.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License (as
+# published by the Free Software Foundation) version 2.1, February 1999.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms and
+# conditions of the GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+##############################################################################
+#
+# This is a template package file for Spack.  We've put "FIXME"
+# next to all the things you'll want to change. Once you've handled
+# them, you can save this file and test your package like this:
+#
+#     spack install synapse-tool
+#
+# You can edit this file again by typing:
+#
+#     spack edit synapse-tool
+#
+# See the Spack documentation for more information on packaging.
+# If you submit this package back to Spack as a pull request,
+# please first remove this boilerplate and all FIXME comments.
+#
+from spack import *
+
+
+class Syntool(CMakePackage):
+    """SYN-TOOL provides a C++ and a python API to read / write neuron
+       connectivity informations. SYN-TOOL is designed to support large
+       connecitivy data with Billions of connections."""
+
+    homepage = "https://bbpcode.epfl.ch/browse/code/hpc/synapse-tool"
+    url      = "ssh://bbpcode.epfl.ch/hpc/synapse-tool"
+
+    version('master', git=url, submodules=True)
+    variant('mpi', default=False, description="Enable MPI backend")
+
+    depends_on('hdf5')
+    depends_on('boost@1.60:')
+    depends_on('python')
+    depends_on('cmake@2.8:', type='build')
+
+    # TODO: submodule update pending
+    patch('hadoken.patch')
+
+    def cmake_args(self):
+        args = []
+        if self.spec.satisfies('+mpi'):
+            args.append('-DSYNTOOL_WITH_MPI=ON')
+        return args
