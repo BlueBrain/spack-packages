c99587e6ce45ec9fe3ac7b5c7c49e7b6ecabdcb9
Pramod S Kumbhar <pramod.r.s.kumbhar@gmail.com>
2017-09-17 23:36:07 +0200

    Prototype Spack packages for Tide application

---
+++ packages/zeroeq/package.py
@@ -0,0 +1,32 @@
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License (as
+# published by the Free Software Foundation) version 2.1, February 1999.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms and
+# conditions of the GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+##############################################################################
+from spack import *
+
+
+class Zeroeq(CMakePackage):
+
+    """Cross-platform C++ library for fast binary and REST messaging"""
+
+    homepage = "https://github.com/HBPVIS/ZeroEQ"
+    url      = "https://github.com/HBPVIS/ZeroEQ.git"
+
+    version('0.8.0',   git=url, tag='0.8.0',      submodules=True)
+    version('0.7.0',   git=url, tag='0.7.0',      submodules=True)
+    version('develop', git=url, submodules=True)
+
+    depends_on('servus')
+    depends_on('boost@1.58:')
+    depends_on('zeromq@4:')
+    depends_on('cmake@3:', type='build')
